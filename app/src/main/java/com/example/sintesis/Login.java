6c03f3db09b7afa873ef515ae4dca3c9af56a14d
Cristian Ramirez <cristian.ramirez.ballestero@gmail.com>
2021-03-27 14:01:18 +0100

    Modal done

--- app/src/main/java/com/example/sintesis/Login.java
+++ app/src/main/java/com/example/sintesis/Login.java
@@ -2,11 +2,14 @@ package com.example.sintesis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
@@ -15,17 +18,23 @@ public class Login extends AppCompatActivity {
     private Button btnRegistrar;
     private Button btnLogin;
 
+    private Dialog dialog;
+    private AlertDialog.Builder dialogBuilder;
+    private TextView tvMensaje;
+    private Button btnAceptar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //Obtener referencia de la parte visual
+        //Obtener referencia de los widgets
         btnRegistrar = findViewById(R.id.btnRegistrarLogin);
         btnLogin = findViewById(R.id.btnLoginLogin);
         etCorreo = findViewById(R.id.etCorreoLogin);
         etPassword = findViewById(R.id.etPasswordLogin);
 
+        //Hacer login onClick
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +42,7 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        //Ir a la activity registrar onClick
         btnRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,25 +52,54 @@ public class Login extends AppCompatActivity {
     }
 
     private void login() {
+        //Obtener referencia de texto de los widgets
         String correo = etCorreo.getText().toString();
         String password = etPassword.getText().toString();
 
+        //Si hay campos vacios muestro modal
         if (correo.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Introduce todos los datos", Toast.LENGTH_SHORT).show();
+            String mensaje = getString(R.string.error_campos_vacios);
+            open_modal(mensaje);
             return;
         }
 
-        //Si hago login correctamente, cambio de activit a dashboard
+        //Si hago login correctamente, cambio de activity a dashboard
         change_activity_to_dashboard();
     }
 
+    //activity_login -> acivity_registrar
     private void change_activity_to_registrar() {
         Intent intent = new Intent(this, Registrar.class);
         startActivity(intent);
     }
 
+    //activity_login -> acivity_dashboard
     private void change_activity_to_dashboard() {
         Intent intent = new Intent(this, Dashboard.class);
         startActivity(intent);
+    }
+
+    private void open_modal(String mensaje) {
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View modalView = getLayoutInflater().inflate(R.layout.activity_error, null);
+
+        //View de los widgets del modal
+        btnAceptar = modalView.findViewById(R.id.btnAceptarError);
+        tvMensaje = modalView.findViewById(R.id.tvMensajeError);
+
+        //Cambiamos el mensaje
+        tvMensaje.setText(mensaje);
+
+        //Cerra modal onClick
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialogBuilder.setView(modalView);
+        dialog = dialogBuilder.create();
+        dialog.show();
     }
 }
--- app/src/main/java/com/example/sintesis/Registrar.java
+++ app/src/main/java/com/example/sintesis/Registrar.java
@@ -2,6 +2,8 @@ package com.example.sintesis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -20,11 +22,18 @@ public class Registrar extends AppCompatActivity {
     private CheckBox cbTerminos;
     private Button btnRegistrar;
 
+
+    private Dialog dialog;
+    private AlertDialog.Builder dialogBuilder;
+    private TextView tvMensaje;
+    private Button btnAceptar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
 
+        //Obtener referencia de los widgets
         txtvLogin = findViewById(R.id.tvLoginRegistrar);
         etNombre = findViewById(R.id.etNombreRegistrar);
         etCorreo = findViewById(R.id.etCorreoRegistrar);
@@ -49,26 +58,32 @@ public class Registrar extends AppCompatActivity {
     }
 
     private void registrar() {
+        //Obtener referencia de texto de los widgets
         String nombre = etNombre.getText().toString();
         String correo = etCorreo.getText().toString();
         String password = etPassword.getText().toString();
         String repetirPassword = etRepetirPassword.getText().toString();
+
+        //Obtner si terminos checked
         boolean terminosAceptados = cbTerminos.isChecked();
 
         //Si campos vacios
         if (nombre.isEmpty() || correo.isEmpty() || password.isEmpty() || repetirPassword.isEmpty()) {
-            Toast.makeText(this, "Introduce todos los datos", Toast.LENGTH_SHORT).show();
+            String mensaje = getString(R.string.error_campos_vacios);
+            open_modal(mensaje);
             return;
         }
         //Si las passwords no coinciden
         if (!password.equals(repetirPassword)) {
-            Toast.makeText(this, "Contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+            String mensaje = getString(R.string.error_passwords_diferentes);
+            open_modal(mensaje);
             return;
         }
 
         //Si no se han aceptado los terminos
         if (!terminosAceptados) {
-            Toast.makeText(this, "No has aceptado los terminos", Toast.LENGTH_SHORT).show();
+            String mensaje = getString(R.string.error_terminos_sin_aceptar);
+            open_modal(mensaje);
             return;
         }
 
@@ -79,5 +94,29 @@ public class Registrar extends AppCompatActivity {
     private void change_activity_to_login() {
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
+    }
+
+    private void open_modal(String mensaje) {
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View modalView = getLayoutInflater().inflate(R.layout.activity_error, null);
+
+        //View de los widgets del modal
+        btnAceptar = modalView.findViewById(R.id.btnAceptarError);
+        tvMensaje = modalView.findViewById(R.id.tvMensajeError);
+
+        //Cambiamos el mensaje
+        tvMensaje.setText(mensaje);
+
+        //Cerra modal onClick
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialogBuilder.setView(modalView);
+        dialog = dialogBuilder.create();
+        dialog.show();
     }
 }
